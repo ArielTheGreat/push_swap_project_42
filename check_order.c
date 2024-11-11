2ca5bc338edfeb8804b7a659e2f900c640558b34
Franco Rocha <franco.rocha@fast2work.de>
2024-11-12 00:29:46 +0100

    applied norminette

--- check_order.c
+++ check_order.c
@@ -12,54 +12,67 @@
 
 #include "push_swap_utils.h"
 
-int check_order_min_to_max(t_list *first_node)
+int	check_order_min_to_max(t_list *first_node)
 {
-    t_list *second_node;
+	t_list	*second_node;
 
-    second_node = first_node->next;
-    while((second_node != NULL) && (*(int *)second_node->content > *(int *)first_node->content))
-    {
-        first_node = first_node->next;
-        second_node = second_node->next;
-    }
-    if (second_node != NULL)
-        return 0;
-    else{
-        return 1;
-    }
+	second_node = first_node->next;
+	while (second_node != NULL
+		&& (*(int *)second_node->content
+			> *(int *)first_node->content))
+	{
+		first_node = first_node->next;
+		second_node = second_node->next;
+	}
+	if (second_node != NULL)
+		return (0);
+	return (1);
 }
 
-void swap(int* a, int* b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
-
-int partition(int* array_int, int low, int high) {
-    int pivot = array_int[high];
-    int i = low - 1;
-
-    for (int j = low; j < high; j++) {
-        if (array_int[j] <= pivot) {
-            i++;
-            swap(&array_int[i], &array_int[j]);
-        }
-    }
-
-    swap(&array_int[i + 1], &array_int[high]);
-    return i + 1;
-}
-
-void quicksort(int* array_int, int low, int high) {
-    if (low < high) {
-        int pi = partition(array_int, low, high);
-
-        quicksort(array_int, low, pi - 1);
-        quicksort(array_int, pi + 1, high);
-    }
-}
-
-void order_array_quicksort(int *array_int,int length_array)
+void	swap(int *a, int *b)
 {
-    quicksort(array_int, 0, length_array - 1);
+	int	temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+int	partition(int *array_int, int low, int high)
+{
+	int	pivot;
+	int	i;
+	int	j;
+
+	pivot = array_int[high];
+	i = low - 1;
+	j = low;
+	while (j < high)
+	{
+		if (array_int[j] <= pivot)
+		{
+			i++;
+			swap(&array_int[i], &array_int[j]);
+		}
+		j++;
+	}
+	swap(&array_int[i + 1], &array_int[high]);
+	return (i + 1);
+}
+
+void	quicksort(int *array_int, int low, int high)
+{
+	int	pi;
+
+	if (low < high)
+	{
+		pi = partition(array_int, low, high);
+		quicksort(array_int, low, pi - 1);
+		quicksort(array_int, pi + 1, high);
+	}
+}
+
+void	order_array_quicksort(int *array_int, int length_array)
+{
+	quicksort(array_int, 0, length_array - 1);
 }
